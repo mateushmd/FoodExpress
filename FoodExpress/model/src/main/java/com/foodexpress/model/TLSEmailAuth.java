9122d807e5dc17814bec8abc5216d4ab5cd8b630
Mateus <chenri48155@gmail.com>
2023-06-24 14:19:38 -0300

    Recuperação de senha #14, Autenticação de e-mail #13, Login do usuário #3

---
+++ FoodExpress/model/src/main/java/com/foodexpress/model/TLSEmailAuth.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.foodexpress.model;
+
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+
+public class TLSEmailAuth {
+
+    private static final String EMAIL = "equipefoodexpress@gmail.com";
+    private static final String PASSWORD = "dwqszhkyydzxctqn";
+
+    protected static Session getSession() {
+        Properties props = new Properties();
+        
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        
+        Authenticator auth = new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(EMAIL, PASSWORD);
+            }
+        };
+        
+        return Session.getInstance(props, auth);
+    }
+}
